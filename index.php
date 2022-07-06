b388c4f8ed3a1ddd072145ecfb997886f9b9cdca
Brahim-tam <Brahim.semmani81@gmail.com>
2022-07-06 11:55:47 -0700

    connection with db and fetch data

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
+
+class Tasks
+{
+    public $id ;
+    public $description ;
+    public $completed ;
+}
+
 try {
 
-    $pdo = new PDO('mysql:host:localhost;dbname:tasks', 'root' , '00001996');
+    $pdo = new PDO('mysql:host=localhost;dbname=tasks;charset=utf8', 'root' , '00001996');
+    $pdo->exec('set names utf8');
+    
 } catch (PDOException $e) {
     die("Errour when try to  connection with db");
 }
+
+$query = $pdo->prepare('SELECT * FROM tasks');
+$query->execute();
+$tasks = $query->fetchAll(PDO::FETCH_CLASS , 'Tasks');
+
